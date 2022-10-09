67ae855f578405357226d48d270a8347eac76743
hex81 <hexin0922@hotmail.com>
2022-10-09 23:38:27 +0800

    update css

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -23,13 +23,15 @@ class LoginForm(FlaskForm):
 
 class RegistrationForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Length(1, 64),
-     Email()])
+                        Email()])
     username = StringField('Username', validators=[
-     DataRequired(), Length(1, 64),
-     Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0, 'Usernames must have only letters, numbers, dots or underscores')])
+                           DataRequired(), Length(1, 64),
+                           Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+                                  'Usernames must have only letters, numbers, dots or underscores')])
     password = PasswordField('Password', validators=[
-     DataRequired(),
-     EqualTo('password2', message='Passwords must match.')])
+                             DataRequired(),
+                             EqualTo('password2',
+                                     message='Passwords must match.')])
     password2 = PasswordField('Confirm password', validators=[DataRequired()])
     submit = SubmitField('Register')
 
